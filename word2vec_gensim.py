29548f1e616e132a307752a75c8d43de1f7cbd53
Amir Hadifar <hadifar.amir@gmail.com>
2018-06-14 11:25:51 +0430

    requirements added

--- word2vec_gensim.py
+++ word2vec_gensim.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
 # Copyright 2017 Amir Hadifar. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,23 +15,29 @@
 # limitations under the License.
 # ==============================================================================
 
+from gensim.models import KeyedVectors
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+OUTPUT_FILE_PATH = './'
+INPUT_FILE_PATH = './wiki.fa.text'
 
 
 def train_model():
-    INPUT_FILE_PATH = './wiki.fa.text'
-    OUTPUT_FILE_PATH = './'
-    model = Word2Vec(LineSentence(INPUT_FILE_PATH), size=200, window=5, cbow=0)
-    model.wv.save(OUTPUT_FILE_PATH + 'word2vec.bin')
+    sentences = LineSentence(INPUT_FILE_PATH)
+    model = Word2Vec(sentences, size=200, window=5, sg=1)
     model.wv.save_word2vec_format(OUTPUT_FILE_PATH + 'word2vec.txt', binary=False)
 
 
 def load_model():
-    wiki_model = Word2Vec.KeyedVectors.load_word2vec_format(OUTPUT_FILE_PATH + 'word2vec.txt')
+    wiki_model = KeyedVectors.load_word2vec_format(OUTPUT_FILE_PATH + 'word2vec.txt')
     most_similar = wiki_model.most_similar(u'ایران')
     for words in most_similar:
         print(words[0])
+
 
 if __name__ == '__main__':
     train_model()
